5b13abf3060e110093059ffc666c0b49cb468a90
Medha Gupta <medha4@gmail.com>
2020-07-09 17:36:24 +0000

    get data

--- definitions.py
+++ definitions.py
@@ -1,1 +1,5 @@
+import requests
 # Feel free to code your definitions here in order to separate your concerns.
+
+data = requests.get("https://covidtracking/api/v1/states/ca/current").json()
+print(data)
