4cd6c110bec400b038289fee55deebd1526ebf8a
Medha Gupta <medha4@gmail.com>
2020-07-09 13:36:29 +0000

    first-commit-for-test

--- calculator.py
+++ calculator.py
@@ -1,5 +1,4 @@
 import definitions
 # The line above will let you separate your concerns by defining functions your calculator might use in a separate file.
-print("running")
 print("Welcome to the Python Calculator\n\nWhat would you like to do?")
 choice = input()
